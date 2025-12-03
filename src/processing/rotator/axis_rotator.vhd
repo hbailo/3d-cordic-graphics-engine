bf8e1b6776cda364b51ab819d52f00bac232ef1e
Hernán Leandro Bailo <hernanbailo@hotmail.com>
2025-12-03 17:26:56 -0300

    Refactor axis rotators in one entity

---
+++ src/processing/rotator/axis_rotator.vhd
@@ -0,0 +1,129 @@
+--! @file
+--! @author Hernán L. Bailo - <hbailo1995@gmail.com>
+--! @date 2025
+library ieee;
+use ieee.numeric_std.all;
+use ieee.std_logic_1164.all;
+
+--! @brief 3D Rotation around cartesian axis
+--! @details
+--! This entity performs a rotation of a 3D vector (xi, yi, zi) around the AXIS axis.
+--!
+--! The input vector is rotated in the plane by the specified angle, leaving the AXIS component unchanged.
+--!
+--! Operation
+--! - Rotation begins when `start` is asserted for at least one rising edge of `clk`.
+--! - When the output vector (xo, yo, zo) becomes valid, output signal 'valid'
+--! is asserted high for one cycle.
+--!
+--! Numerical Representation
+--! - Cartesian coordinates (x, y, z):
+--!   - Format:    Two's complement signed number
+--!   - Bit width: DATA_WIDTH bits: (1 sign bit + (DATA_WIDTH - 1) magnitude bits)
+--!   - Range:     [-2^(DATA_WIDTH - 1), 2^(DATA_WIDTH - 1) - 1]
+--!
+--! - Angle:
+--!   - Format:     Q0.(DATA_WIDTH - 1) fixed-point scaled radians.
+--!   - Bit width:  DATA_WIDTH bits (1 sign bit + (DATA_WIDTH - 1) fractional bits)
+--!   - Range:      [-π, π) where π = 2^(DATA_WIDTH - 1)
+--!   - Resolution: π / 2^(DATA_WIDTH - 1) radians per LSB
+--!   - Encoding:   z_actual = z_encoded * π / 2^(DATA_WIDTH - 1), where z_actual is in
+--!                 radians
+entity axis_rotator is
+    generic (
+        --! Coordinates and angles bit width
+        DATA_WIDTH : positive range 1 to 1023;
+        AXIS       : character
+    );
+    
+    port (
+        --! System clock
+        clk : in std_logic;
+
+        --! Active-high asynchronous reset
+        rst : in std_logic;
+
+        --! Start signal
+        start : in std_logic;
+
+        --! X coordinate input
+        xi : in std_logic_vector(DATA_WIDTH - 1 downto 0);
+
+        --! Y coordinate input
+        yi : in std_logic_vector(DATA_WIDTH - 1 downto 0);
+
+        --! Z coordinate input
+        zi : in std_logic_vector(DATA_WIDTH - 1 downto 0);
+
+        --! Axis rotation angle
+        angle : in std_logic_vector(DATA_WIDTH - 1 downto 0);
+
+        --! X coordinate output
+        xo : out std_logic_vector(DATA_WIDTH - 1 downto 0);
+
+        --! Y coordinate output
+        yo : out std_logic_vector(DATA_WIDTH - 1 downto 0);
+
+        --! Z coordinate output
+        zo : out std_logic_vector(DATA_WIDTH - 1 downto 0);
+
+        --! Valid output flag
+        valid : out std_logic
+    );
+end entity;
+
+--! @brief Structural architecture
+--! @details
+--! This architecture uses a `cordic` entity to rotate the components around
+--! the selected axis.
+architecture structural of axis_rotator is
+    signal ui : std_logic_vector(xi'range);
+    signal vi : std_logic_vector(yi'range);
+    signal uo : std_logic_vector(xo'range);
+    signal vo : std_logic_vector(yo'range);
+begin
+    axis_gen: case AXIS generate
+        when 'X' =>
+            ui <= yi;
+            vi <= zi;
+            xo <= xi;
+            yo <= uo;
+            zo <= vo;
+            
+        when 'Y' =>
+            ui <= zi;
+            vi <= xi;
+            xo <= vo;            
+            yo <= yi;
+            zo <= uo;            
+            
+        when 'Z' =>
+            ui <= xi;
+            vi <= yi;
+            xo <= uo;            
+            yo <= vo;
+            zo <= zi;
+
+        when others =>
+            assert false 
+                report "Invalid AXIS value (must be 'X','Y','Z')" 
+                severity failure;
+    end generate;
+
+    plane_rotator: entity work.cordic
+        generic map (
+            DATA_WIDTH => DATA_WIDTH
+        )
+        port map (
+            clk   => clk,
+            rst   => rst,
+            start => start,
+            xi    => ui,
+            yi    => vi,
+            zi    => angle,
+            xo    => uo,
+            yo    => vo,
+            zo    => open,
+            valid => valid
+        );
+end architecture;
